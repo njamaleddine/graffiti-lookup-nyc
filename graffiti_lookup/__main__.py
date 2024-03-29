f6dd328d75e3c6df1bc2fdc061262d6bf6c59dbc
Nabil Jamaleddine <njamaleddine@users.noreply.github.com>
2024-03-28 22:53:30 -0400

    feat: add ability to merge output file with fetched results (#2)

--- graffiti_lookup/__main__.py
+++ graffiti_lookup/__main__.py
@@ -26,14 +26,46 @@ parser.add_argument(
     help="The output file path for the requested graffiti service request records",
 )
 parser.add_argument(
-    "-t",
-    "--file-type",
-    choices=SUPPORTED_FILE_TYPES,
-    help="The output file type"
+    "-m",
+    "--merge-file",
+    action="store_true",
+    help="Merge graffiti request records with existing file",
+)
+parser.add_argument(
+    "-t", "--file-type", choices=SUPPORTED_FILE_TYPES, help="The output file type"
 )
 
-
 args = parser.parse_args()
+
+
+def read_file(file_path, file_type, result, fieldnames):
+    with open(file_path, "r") as file:
+        if file_type == "json":
+            return json.loads(file.read())
+        elif file_type == "csv":
+            csv_reader = csv.DictReader(file, fieldnames=fieldnames)
+            return [row for row in csv_reader][1:]
+        else:
+            sys.stderr.write(
+                f"Unsupported file-type {file_type} not in {SUPPORTED_FILE_TYPES}"
+            )
+
+
+def write_file(file_path, file_type, result, fieldnames):
+    with open(file_path, "w") as file:
+        if file_type == "json":
+            file.write(json.dumps(result, indent=4))
+        elif file_type == "csv":
+            csv_writer = csv.DictWriter(file, fieldnames=fieldnames)
+            csv_writer.writeheader()
+            if args.ids:
+                csv_writer.writerows(result)
+            else:
+                csv_writer.writerow(result)
+        else:
+            sys.stderr.write(
+                f"Unsupported file-type {file_type} not in {SUPPORTED_FILE_TYPES}"
+            )
 
 
 async def main():
@@ -57,20 +89,16 @@ async def main():
     if not file_path:
         sys.stdout.write(json.dumps(result))
     else:
-        with open(file_path, "w") as file:
-            if file_type == "json":
-                file.write(json.dumps(result, indent=4))
-            elif file_type == "csv":
-                csv_writer = csv.DictWriter(file, fieldnames=fieldnames)
-                csv_writer.writeheader()
-                if args.ids:
-                    csv_writer.writerows(result)
-                else:
-                    csv_writer.writerow(result)
-            else:
-                sys.stderr.write(
-                    f"Unsupported file-type {file_type} not in {SUPPORTED_FILE_TYPES}"
-                )
+        if args.merge_file:
+            file_results = read_file(file_path, file_type, result, fieldnames)
+            file_result_map = {
+                row.get(GraffitiLookup.ID_FIELD): row for row in file_results
+            }
+            result_map = {row.get(GraffitiLookup.ID_FIELD): row for row in result}
+            all_results = {**file_result_map, **result_map}
+            result = list(all_results.values())
+
+        write_file(file_path, file_type, result, fieldnames)
 
 
 if __name__ == "__main__":
